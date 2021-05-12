0fcc2c95d71a1c67a70108988bf19fc20c420b8a
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2021-05-12 12:28:08 +0200

    Add opdracht Array Methods deel 1

---
+++ script.js
@@ -0,0 +1,63 @@
+// A
+  const addTheWordCool = function(array){
+    array.push("cool");
+    return array;
+}
+console.log("Add cool:",addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
+
+// B
+const amountOfElementsInArray = (fruits) => fruits.length
+
+
+console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+// 3
+
+// C
+const selectBelgiumFromBenelux = (benelux) => benelux[0];
+
+console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"])); 
+// resultaat: "Belgie" 
+
+// D
+const lastElementInArray = (dieren) => dieren[dieren.length - 1]
+
+console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])); 
+// resultaat: "Schildpad"
+
+// E
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+  
+  const impeachTrumpSlice = function(presidents ) {
+    return presidents.slice(1);
+  }
+  const impeachTrumpSplice = function(presidents) {
+    return presidents.splice(1);
+  }
+  
+  console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+  console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+  // Het verschil:
+  // .slice() je verwijderd een element en retourneert de rest 
+  // .splice() je kan iets verwijderen, maar ook vervangen, een nw element 
+  // plaatsen en dan retourneren
+
+// F
+stringsTogether = (string) => string.join()
+
+console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}'])) 
+//resultaat: "Winc Academy is leuk ;-}"
+
+// G
+combineArrays = (array1, array2) => {
+  const array3 = [].concat(array1, array2);
+  return array3;
+}
+
+console.log(combineArrays([1,2,3], [4,5,6]))
+// resultaat: [1,2,3,4,5,6]
+
+
+
+
